085252b600f3bf0f03eebecb579dca36a4aecd08
AinurWin <ainurbogdanov69@gmail.com>
2025-07-02 05:42:32 +0400

    copyed footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/components/header/header/header.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
   ],
   imports: [
+    SharedModule,
     BrowserModule,
     AppRoutingModule,
   ],

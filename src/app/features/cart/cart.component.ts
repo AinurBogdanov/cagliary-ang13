648e298afb870a9eb946a177c5c394d84f50760d
AinurWin <ainurbogdanov69@gmail.com>
2025-07-13 07:28:20 +0400

    added promo

--- src/app/features/cart/cart.component.ts
+++ src/app/features/cart/cart.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss'],
 })
-export class CartComponent implements OnInit {
-  constructor() {}
+export class CartComponent {
+  selectedDrink: string = 'cola';
+  promoSelected: boolean = true;
 
-  ngOnInit(): void {}
+  selectDrink(val: string) {
+    this.selectedDrink = val;
+  }
+  togglePromo(event: Event) {
+    this.promoSelected = !this.promoSelected;
+    console.log(this.promoSelected);
+    event.stopPropagation();
+  }
 }
